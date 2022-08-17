309df40b0d97fc159cbe29a1f060a828ef65ac44
BlurplePanda <bootham@wgc.school.nz>
2022-08-17 15:18:41 +1200

    Changed connection to correct database

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost", "bootham", "richpatch76", "bootham_canteen");
+$con = mysqli_connect("localhost", "bootham", "richpatch76", "bootham_fundraisers");
 if(mysqli_connect_errno()){
    echo "Failed to connect to MySQL:".mysqli_connect_error(); die();}
 
