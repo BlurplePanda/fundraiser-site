bd30c02c3eec0df0a9f0ef1aec0b703e2e8c3c94
BlurplePanda <bootham@wgc.school.nz>
2022-08-23 12:23:58 +1200

    Added code/queries to insert donor and pledge information, and added error/success messages

--- insertpledge.php
+++ insertpledge.php
@@ -2,6 +2,35 @@
 $con = mysqli_connect("localhost", "bootham", "richpatch76", "bootham_fundraisers");
 if(mysqli_connect_errno()){
     echo "Failed to connect to MySQL:".mysqli_connect_error(); die();}
+
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$email = $_POST['email'];
+$amount = $_POST['amount'];
+$page = $_POST['page'];
+$fromurl = $_POST['url'];
+
+$insert_donor = "INSERT INTO donors (DonorFName, DonorLName, DonorEmail) VALUES ('$fname', '$lname', '$email')";
+if(mysqli_query($con, $insert_donor)){
+    $donorinsert = true;
+    $get_inserted_id_query = "SELECT DonorID FROM donors WHERE DonorFName LIKE '$fname' AND DonorLName LIKE '$lname' AND DonorEmail LIKE '$email'";
+    $get_inserted_id_result = mysqli_query($con, $get_inserted_id_query);
+    $get_inserted_id_record = mysqli_fetch_assoc($get_inserted_id_result);
+    $id = $get_inserted_id_record['DonorID'];
+
+    $insert_pledge = "INSERT INTO pledges (DonorID, PageID, PledgeAmount) VALUES ($id, $page, $amount)";
+    if (!mysqli_query($con, $insert_pledge)){
+        $pledgeinsert = false;
+    }
+    else {
+        $pledgeinsert = true;
+    }
+}
+else {
+    $donorinsert = false;
+}
+
+header("refresh: 3; url=$fromurl");
 ?><!DOCTYPE html>
 
 <html lang='en'>
@@ -24,10 +53,16 @@ if(mysqli_connect_errno()){
 </header>
 
 <main>
-    <h1>Make a pledge</h1>
-    
-
-
+    <?php
+    if($pledgeinsert && $donorinsert) {
+        echo "<h1>Success!</h1>
+              <p>Pledge successfully made :)";
+    }
+    else {
+        echo "<h1>Uh oh!</h1>
+              <p>Looks like that didn't work. Please try again.";
+    }
+    ?>
 </main>
 </body>
 
