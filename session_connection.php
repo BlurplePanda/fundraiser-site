a24257081347f36c25456ef498b6aa99f7fe5b00
BlurplePanda <bootham@wgc.school.nz>
2022-08-29 12:32:11 +1200

    Added "session_start()" and renamed to show this

--- session_connection.php
+++ session_connection.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 $con = mysqli_connect("localhost", "bootham", "richpatch76", "bootham_fundraisers");
 if(mysqli_connect_errno()){
     echo "Failed to connect to MySQL:".mysqli_connect_error(); die();}
