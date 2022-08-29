3b8895273f0d8122fa8488807cd6bd505c645ce5
BlurplePanda <bootham@wgc.school.nz>
2022-08-29 12:52:41 +1200

    Added basic error page

---
+++ error_page.php
@@ -0,0 +1,24 @@
+<?php include 'session_connection.php';
+?><!DOCTYPE html>
+
+<html lang='en'>
+
+<head>
+    <title> Rundfaise </title>
+    <meta charset='utf-8'>
+    <link rel='stylesheet' type='text/css' href='style.css'>
+</head>
+
+<body>
+<header>
+    <?php include 'header.php'?>
+</header>
+
+<main>
+    <h1>Oops!</h1>
+    <p>You have to log in to access that page.</p>
+    <?php header("refresh:2, url=login.php")?>
+</main>
+</body>
+
+</html>
