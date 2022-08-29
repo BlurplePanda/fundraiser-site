745153331d848a9602ab5f2dbc5a7f082fdcb4fd
BlurplePanda <bootham@wgc.school.nz>
2022-08-29 12:46:43 +1200

    Added page to process "log-outs"

---
+++ process_logout.php
@@ -0,0 +1,28 @@
+<?php
+include 'session_connection.php';
+?><!DOCTYPE html>
+
+<html lang='en'>
+
+<head>
+    <title> Rundfaise </title>
+    <meta charset='utf-8'>
+    <link rel='stylesheet' type='text/css' href='style.css'>
+</head>
+
+<body>
+<header>
+    <?php include 'header.php'?>
+</header>
+
+<main>
+    <?php
+    session_destroy();
+    echo "<h1> Logged out! </h1>";
+    echo "<p> Redirecting...";
+    header("refresh:1, url=login.php")
+    ?>
+</main>
+</body>
+
+</html>
