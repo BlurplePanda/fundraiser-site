cf4adc36b9479c626a4e9e100a1dc5f1ad5d895b
BlurplePanda <bootham@wgc.school.nz>
2022-09-04 15:46:41 +1200

    Created error page for if logged in but wrong account

---
+++ account_error_page.php
@@ -0,0 +1,24 @@
+<?php include 'session_connection.php';
+?><!DOCTYPE html>
+
+<html lang='en'>
+
+<head>
+    <title> Rundfaise </title>
+    <meta charset='utf-8'>
+    <link rel='stylesheet' type='text/css' href='styles.css'>
+</head>
+
+<body>
+<header>
+    <?php include 'header.php'?>
+</header>
+
+<main>
+    <h1>Oops!</h1>
+    <p>That's not your campaign. Make sure you are logged in to the right account.</p>
+    <?php header("refresh:3, url=user_campaigns.php")?>
+</main>
+</body>
+
+</html>
