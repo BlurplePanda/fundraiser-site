57bab8e84d179c25a408861e470c9fb453ba284f
BlurplePanda <bootham@wgc.school.nz>
2022-09-16 09:38:46 +1200

    Changed displayed"title" from fundraiser name to page name

--- user_campaigns.php
+++ user_campaigns.php
@@ -15,7 +15,7 @@ if (!isset($_SESSION['user'])) {
     <title> My campaigns - Rundfaise </title>
     <meta charset='utf-8'>
     <link rel='stylesheet' type='text/css' href='styles.css'>
-    <!--  -->
+    <!-- Small js function to confirm deletion -->
     <script type='text/javascript'>
         function openurl(newurl) {
             if (confirm("Are you sure you want to delete this page?")) {
@@ -34,7 +34,7 @@ if (!isset($_SESSION['user'])) {
     <h1>My campaigns</h1>
     <?php
     // Gets all of a specific fundraiser's campaigns
-    $my_campaigns_query = "SELECT pages.PageID, pages.PageGoal, pages.PageImage, fundraisers.FRFName, fundraisers.FRLName
+    $my_campaigns_query = "SELECT pages.PageID, pages.PageName, pages.PageGoal, pages.PageImage
                         FROM pages, fundraisers
                         WHERE pages.FundraiserID = fundraisers.FundraiserID AND fundraisers.FundraiserID = '$user'
                         ORDER BY pages.PageGoal";
@@ -46,9 +46,11 @@ if (!isset($_SESSION['user'])) {
         echo "<div class='all-campaigns-container'>";
         // For each campaign
         while ($my_campaigns_record = mysqli_fetch_assoc($my_campaigns_result)) {
-            // Display (the icon, name, and goal of) said campaign
+            // Display (the title, icon, and goal of) said campaign
             echo "<div class='campaign'>";
-            echo "<p><a href='campaign.php?id=" . $my_campaigns_record['PageID'] . "&fromurl=" . $_SERVER['REQUEST_URI'] . "'><img src='images/" . $my_campaigns_record['PageImage'] . "' alt='' class='allcampaignsimage'><br>" . $my_campaigns_record['FRFName'] . " " . $my_campaigns_record['FRLName'] . "'s fundraiser<br>";
+            echo "<p><a href='campaign.php?id=" . $my_campaigns_record['PageID'] . "&fromurl=" . $_SERVER['REQUEST_URI']
+                . "'><img src='images/" . $my_campaigns_record['PageImage'] . "' alt='' class='allcampaignsimage'><br>"
+                . $my_campaigns_record['PageName'] . "<br>";
             echo $my_campaigns_record['PageGoal'] . "</a>";
 
             // Link to edit said campaign
