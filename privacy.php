4db833c76811e8887626029e77ae748aa7e5463a
BlurplePanda <bootham@wgc.school.nz>
2022-09-27 21:49:30 +1300

    Created and styled privacy policy page

---
+++ privacy.php
@@ -0,0 +1,38 @@
+<?php include 'session_connection.php'?><!DOCTYPE html>
+
+<html lang='en'>
+
+<head>
+    <title> Rundfaise </title>
+    <meta charset='utf-8'>
+    <link rel='stylesheet' type='text/css' href='styles.css'>
+</head>
+
+<body>
+<header>
+    <?php include 'header.php' ?>
+</header>
+
+<main>
+    <h1>Privacy</h1>
+    <p>We take privacy seriously here at Rundfaise and do our best to let users know about our policies according to the
+    New Zealand Privacy Act. Here are the ways we manage your data:</p>
+    <ul>
+        <li>We (Rundfaise) collect personal data when You (the user) make a pledge to a campaign (as a Donor) or when
+            You create an account (as a Fundraiser). This data includes first and last names and email addresses.</li>
+        <li>The data We collect is mainly used for administrative purposes, by Rundfaise staff only. Under
+            certain circumstances, personal data of a Donor may be revealed to the Fundraiser of the campaign.
+            We do not share Your data with any advertising providers or other third-party bodies.</li>
+        <li>We use session cookies. These are necessary in order for the site to function correctly and
+            securely, and they are automatically deleted when You close Your browser.</li>
+        <li>All personal data collected by Rundfaise is required in order for Us to perform Our purpose as a business
+            and website. Pledges and accounts cannot be created without the entry of personal data. As such, if data is
+            not voluntary supplied, certain important functions of our website will not be available to You.</li>
+        <li>You may request access and/orcorrection of Your personal data being held by Us at any time. This is possible
+            through Our contact email address: <a href='mailto:bootham@wgc.school.nz'>bootham@wgc.school.nz</a>.</li>
+        <li>All registered users will be notified if Our privacy policy changes. We will contact you through the email
+            address that You have provided Us to inform You of the new ways We will process your data.</li>
+    </ul>
+
+</main>
+</body>
